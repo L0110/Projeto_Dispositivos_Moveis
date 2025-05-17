090c63fab85200bcea070d5c13e40fead8dc7b6a
Filipe Anunciação <lipe_z@outlook.com>
2025-05-17 19:44:34 -0300

    Introdução das ferramentas expo.dev

--- bets-friends/App.js
+++ bets-friends/App.js
@@ -8,3 +8,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { View,SafeAreaView , Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+
+/*
+cor 01: #F3F4F0 (tela principal)
+cor 02: #8A919B (marcadores/seletores)
+cor 03: #F0FF69 (botão area escura)
+cor 04: #776C5B (botão) area clara)
+cor 05: #27262D (apoio/contraste)
+*/
