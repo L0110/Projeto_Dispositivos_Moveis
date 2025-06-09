e48609df0d01e8e10274e1b36f2e701307550bb0
Filipe Anunciação <lipe_z@outlook.com>
2025-06-08 22:47:50 -0300

    restauracao Revert "Inclusao Game03"

---
+++ bets-friends/src/pages/jogos/Game05.js
@@ -0,0 +1,6 @@
+/*
+Game quem aperta o botão primeiro
+- Exige comunicacao entre dois jogadores
+- Tela inicial com botão de iniciar o jogo
+- Tela de jogo com botão que conta em quantos segundos o jogador apertou o botão
+*/
